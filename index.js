65bf33628aeadf73a7902108157e60f5d5888746
Raphael <rmorgansizemore@gmail.com>
2023-03-31 22:45:29 -0700

    basic vector2 and vector3
    made classes `Vector2` and `Vector3` that are so far just Arrays except they have to be 2 and 3 values, added `VectorX.isVectorX()` function for both just like (and utilizing) `Array.isArray()`

---
+++ index.js
@@ -0,0 +1,26 @@
+class Vector2 extends Array {
+    constructor(x,y) {
+        if (x == undefined) return super(0,0);
+        if (y == undefined) return super(x,0);
+        return super(x,y);
+    }
+}
+class Vector3 extends Array {
+    constructor(x,y,z) {
+        if (x == undefined) return super(0,0,0);
+        if (y == undefined) return super(x,0,0);
+        if (z == undefined) return super(x,y,0);
+        return super(x,y);
+    }
+}
+Vector2.isVector2 = function(variable) {
+    if (variable instanceof Vector2) return true;
+    if (Array.isArray(variable) && variable.length === 2) return (!variable.filter(x => (typeof x != "number")).length)
+    return false;
+}
+Vector3.isVector3 = function(variable) {
+    if (variable instanceof Vector3) return true;
+    if (Array.isArray(variable) && variable.length === 3) return (!variable.filter(x => (typeof x != "number")).length)
+    return false;
+}
+module.exports = {Vector2,Vector3}
