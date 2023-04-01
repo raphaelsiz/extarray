645f5c40a5aed990041ba2d03c1eb295280cfdf6
Raphael <rmorgansizemore@gmail.com>
2023-03-31 22:47:51 -0700

    updated to module

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-class Vector2 extends Array {
+export class Vector2 extends Array {
     constructor(x,y) {
         if (x == undefined) return super(0,0);
         if (y == undefined) return super(x,0);
         return super(x,y);
     }
 }
-class Vector3 extends Array {
+export class Vector3 extends Array {
     constructor(x,y,z) {
         if (x == undefined) return super(0,0,0);
         if (y == undefined) return super(x,0,0);
@@ -23,4 +23,3 @@ Vector3.isVector3 = function(variable) {
     if (Array.isArray(variable) && variable.length === 3) return (!variable.filter(x => (typeof x != "number")).length)
     return false;
 }
-module.exports = {Vector2,Vector3}
